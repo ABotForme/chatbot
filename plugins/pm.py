487e7ca2510ad6ca428e80ef215b441d1216cc78
ABotForme <2020844177@qq.com>
2022-02-20 13:44:00 +0800

    Add files via upload

--- plugins/pm.py
+++ plugins/pm.py
@@ -44,6 +44,26 @@ async def pm_sticker(bot, message):
             message_id=message.message_id
         )
 
+@Client.on_message(filters.private & filters.animation)
+#过滤出表情包2，回复/
+async def pm_animation(bot, message):
+    if message.from_user.id == Config.ADMIN:
+        await bot.send_message(
+            chat_id=int(message.chat.id),
+            message_id=message.message_id
+        )
+        info = await bot.get_users(user_ids=message.from_user.id)
+        reference_id = int(message.chat.id)
+        await bot.send_message(
+            chat_id=Config.ADMIN,
+            text=Presets.PM_TXT_ATT.format(reference_id, info.first_name),
+            parse_mode="html"
+        )
+        await bot.send_message(
+            chat_id=Config.ADMIN,
+            message_id=message.message_id
+        )
+        
 @Client.on_message(filters.private & filters.media)
 #过滤出媒体，回复/
 async def pm_media(bot, message):
